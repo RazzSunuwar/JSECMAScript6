2752de5a70dbbd311d617d6ce32d0265bf75b0f8
RazzSunuwar <razzkoinch5@gmail.com>
2020-01-05 14:39:39 +0545

    First Commit

---
+++ es6.js
@@ -0,0 +1,42 @@
+// JavaSCript let
+// This statement allows you to declare a variable with block scope.
+
+// JavaSCript const
+// The const statement allows you to declare a constant (a JavaScript variable with a constant value).
+// Constants are similar to let variables, except that the value cannot be changed.
+
+// JavaScript Arrow function
+// Arrow function allows a short syntax for writting function expressions.
+// you don't need the function keyword, the return keyword, and the curly brackets.
+// Arrow function do not have their own "this". They are not well suited for defining object methods.
+// Arrow function are not hoisted. They must be defined before they are used.
+// Using const is safer than using var, because a function expression is always constant value.
+// You can only omit the return keyword and the curly brackets if the function is a single statement.
+// Because of this, it might be a good habit to always keep them:
+
+// JavaScript Classes
+// ES6 introduced classes.
+// A class is a type of function, but instead of using the keyword function to initiate it, we
+// use the keyword class, and the properties are assigned inside a constructor() method.
+// Use the keyword class to create a class, and always add a constructor method.
+// The constructor method is called each time the class object is initialized.
+
+// JavaScript Array.find()
+// The find() method returns the value of the first array element that passes a test function.
+
+// Note that the function takes 3 arguments:
+// The item value
+// The item index
+// The array itself
+
+// JavaScript ArrayIndex.find()
+// The find() method returns the index of the first array element that passes a test function.
+
+// JavaScript New Number Properties
+// ES6 added the following properties to the Number object:
+// EPSILON
+// MIN_SAFE_INTEGER
+// MAX_SAFE_INTEGER
+
+
+
